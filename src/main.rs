c4f2cfb684e99d2bfcbddc5d32f8419c7ae389ca
cjglo <chris.j.gallo3@gmail.com>
2022-07-13 20:39:07 -0400

    added extra route to practice

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,30 @@ use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
 
 
-async fn welcome_message(req: HttpRequest) -> impl Responder {
+async fn health_check(req: HttpRequest) -> impl Responder {
     
     //let username = req.match_info().get("username");
 
-    format!("Hello!")
+    format!("Healthy and Here!")
+}
+
+
+async fn use_code(req: HttpRequest) -> impl Responder {
+    
+    let id = req.match_info().get("id").unwrap_or("noId");
+    let code = req.match_info().get("code").unwrap_or("noCode");
+
+    /* let id = match id {
+        Some(id) => id,
+        None => return format!("No Valid Id")
+    };
+    let code = match code {
+        Some(code) => code,
+        None => return format!("No Doce Code!")
+    };
+    */
+
+    format!("code was: {code}, and id was {id}")
 }
 
 
@@ -15,7 +34,8 @@ async fn main() -> std::io::Result<()> {
     
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(welcome_message))
+            .route("/", web::get().to(health_check))
+            .route("/pur", web::get().to(use_code))
     })
     .bind("127.0.0.1:8000")?
     .run()
